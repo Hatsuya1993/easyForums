7a58586e6104745fb778d1b64581140b951a4140
Hatsuya1993 <60688830+Hatsuya1993@users.noreply.github.com>
2020-12-29 12:17:52 +0800

    Next fixing the null value for results

--- controller/forumsController.js
+++ controller/forumsController.js
@@ -1,12 +1,82 @@
+const db = require('../connection')
+
 const home = (req, res) => {
-    res.render('home')
+    let sql = 'SELECT * FROM forumDetails';
+    db.query(sql, (err, results) => {
+        if (err) {
+            throw err
+        }
+        res.render('home', {
+            results
+        })
+    })
 }
 
 const add = (req, res) => {
-    console.log(req.body)
+    const {
+        email,
+        topic,
+        textArea
+    } = req.body
+    let sql = 'INSERT INTO forumDetails (email, topic, text) VALUES (?, ?, ?)';
+    db.query(sql, [email, topic, textArea], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.redirect('/')
+    })
+}
+
+const deleteId = (req, res) => {
+    const {
+        id
+    } = req.params
+    let sql = `DELETE FROM forumDetails WHERE id = ${id}`
+    db.query(sql, (err, results) => {
+        if (err) {
+            throw err
+        }
+        res.redirect('/')
+    })
+}
+
+const forumId = (req, res) => {
+    const {
+        id
+    } = req.params
+    // let sql = `SELECT * FROM forumDetails WHERE id = ${id}`
+    let sql =  `SELECT email, topic, text, answer FROM forumdetails INNER JOIN answer WHERE forumId = ${id}`
+    db.query(sql, (err, results) => {
+        if (err) {
+            throw err
+        }
+        console.log(id, results)
+        res.render('forumPage', {
+            results,
+            id
+        })
+    })
+
+}
+
+const answerForum = (req, res) => {
+    const {
+        textArea
+    } = req.body
+    const {id} = req.params
+    let sql = 'INSERT INTO answer (answer, forumId) VALUES (?, ?)';
+    db.query(sql, [textArea, id], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.redirect(`/forum/${id}`)
+    })
 }
 
 module.exports = {
     home,
-    add
+    add,
+    deleteId,
+    forumId,
+    answerForum
 }
--- routes/forumRoutes.js
+++ routes/forumRoutes.js
@@ -2,11 +2,16 @@ const express = require('express')
 
 const forumRoutes = express.Router()
 
-const {home, add} = require('../controller/forumsController')
+const {home, add, deleteId, forumId, answerForum} = require('../controller/forumsController')
 
 forumRoutes.route('/').get(home)
 
 forumRoutes.route('/add').post(add)
 
+forumRoutes.route('/delete/:id').get(deleteId)
+
+forumRoutes.route('/forum/:id').get(forumId)
+
+forumRoutes.route('/answer/:id').post(answerForum)
 
 module.exports = forumRoutes
